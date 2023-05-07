91f4dda4f19aa3fd56f9ae2ae11be2861ce12e69
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-07 13:30:22 +0800

    attempt fix cors

--- server.js
+++ server.js
@@ -7,13 +7,8 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors("http://localhost:3000/"));
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true});
