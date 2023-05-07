667adaaad7d839cdd35cc30e29abb3dec8ac76d2
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-07 13:53:50 +0800

    attempt fix cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 //app.use(cors("http://localhost:3000/"));
 app.use(express.json());
 app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
     res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
     if (req.method == "OPTIONS") {
         return res.sendStatus(200);
