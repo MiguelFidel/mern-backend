908daafcf951d66dbe368661fb8dccb5daeff757
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-07 13:40:34 +0800

    fix cors

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(cors("http://localhost:3000/"));
 app.use(express.json());
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    if (req.method == "OPTIONS") {
+      return res.sendStatus(200);
+    }
+    next();
+  });
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {useNewUrlParser: true});
