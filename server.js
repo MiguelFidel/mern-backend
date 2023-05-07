9ab21b9c0df3653f3798ba42126846cfd644e490
MiguelFidel <kazuki.accel11@gmail.com>
2023-05-07 13:45:40 +0800

    fixed cors

--- server.js
+++ server.js
@@ -11,10 +11,7 @@ app.use(cors("http://localhost:3000/"));
 app.use(express.json());
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000/");
-    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-    if (req.method == "OPTIONS") {
-      return res.sendStatus(200);
-    }
+    res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE");
     next();
   });
 
